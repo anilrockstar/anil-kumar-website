ed2dcb15f82506657e7e8d31f97daed068695deb
anilrockstar <40191495+anilrockstar@users.noreply.github.com>
2023-10-01 14:16:39 +0000

    made some changes with weather data and all

--- app.py
+++ app.py
@@ -8,15 +8,15 @@ app = Flask(__name__)
 tech_skills =[
   "Python","SQL","HTML","CSS","JavaScript"
 ]
-url = "https://api.weatherapi.com/v1/current.json?key=433cf5b0f8d74973ab3195705233009&q=Hyderabad"
-
-response = requests.get(url)
-
-all_data = response.text
-json_data = json.loads(all_data)  
+def weather_data():
+  url = "https://api.weatherapi.com/v1/current.json?key=433cf5b0f8d74973ab3195705233009&q=Hyderabad"
+  response = requests.get(url)
+  json_data = json.loads(response.text)
+  return json_data
 
 @app.route("/")
 def hello_world():
+  json_data = weather_data()
   return render_template('home.html',skills=tech_skills,weather=json_data)
 
 if __name__ == '__main__':
