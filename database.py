f06ed4e4550084eb19d3dd26154b0c491d67e7d4
anilrockstar <40191495+anilrockstar@users.noreply.github.com>
2023-10-02 22:55:48 +0530

    Update database.py deleted passwrd username

--- database.py
+++ database.py
@@ -1,6 +1,6 @@
 from sqlalchemy import create_engine, text
 
-db_connection_string = "mysql+pymysql://xwwxp13nhdxr9kjrrvs0:pscale_pw_38C82E9SWU5x3CLRrA85Xq58HT82Wbx7gdlj3QAxsSF@aws.connect.psdb.cloud/website_data?charset=utf8mb4"
+db_connection_string = "mysql+pymysql://@aws.connect.psdb.cloud/website_data?charset=utf8mb4"
 engine = create_engine(db_connection_string, connect_args={
         "ssl": {
              "ssl_ca": "/etc/ssl/cert.pem"
